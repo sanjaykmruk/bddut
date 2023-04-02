bcb556d8dfb48b4bcda05453bb1d0410bb55c04b
sanjaykmruk <sjkmruk@gmail.com>
2023-04-03 03:11:50 +0530

    initial commit

---
+++ src/main/java/com/bdd/ut/BddutApplication.java
@@ -0,0 +1,13 @@
+package com.bdd.ut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddutApplication.class, args);
+	}
+
+}
